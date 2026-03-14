a133d87b238e715348bc93de10f2038b69cc507b
Pratap Kumar <kumarpratap6969@gmail.com>
2026-03-14 19:37:48 +0530

    Add files via upload

---
+++ Typecasting.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Typecasting {
+    public static void main(String[] args){
+    Scanner input= new Scanner(System.in);
+    float num=input.nextFloat();
+    System.out.println(num);
+
+    
+    // input.close();
+
+    // int num=(int)(94.45);
+    // System.out.println(num);
+    // }
+
+    // System.out.println("你好");
+    // System.out.println(3*9);
+    input.close();
+
+    
+    }
+    
+}
